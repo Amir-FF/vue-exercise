dec786852113ed9f52ea412af5f94efdc6a6c228
Amir-FF <amir.computer.04.ir@gmail.com>
2026-09-03 02:44:01 +0330

    add reactivity

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <script setup>
-  const massage = "hello would";
+  import { reactive, ref } from "vue";
+
+  const number = ref(2);
+  const array = reactive([2, 4, 6]);
+
+  const find = array.find((arr) => {
+    return arr === 6;
+  });
+
+  const upadateNubmber = () => {
+    setTimeout(() => {
+      number.value = 4;
+    }, 3000);
+  };
+  upadateNubmber();
 </script>
 
 <template>
-  <h1>{{ massage }}</h1>
+  <h1>number: {{ number }}</h1>
+  <h1>array: {{ array }}</h1>
+  <h1>find: {{ find }}</h1>
 </template>
