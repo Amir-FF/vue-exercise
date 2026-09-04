0b20ea2204d600c1ed90d75da149f060be426176
Amir-FF <amir.computer.04.ir@gmail.com>
2026-09-04 20:47:44 +0330

    add template ref

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,24 @@
 <script setup>
   import { computed, ref, watch } from "vue";
 
-  const name = ref("ali");
-  const num = ref(0);
-  const firstName = ref("amir");
-  const lastName = ref("shamiri");
-
-  watch(num, (newValue, oldValue) => {
-    console.log(oldValue, newValue);
-  });
-
-  const getFullName = computed((prev) => {
-    console.log("prev value, ", prev);
-    console.log("getFullName was called");
-    return firstName.value + " " + lastName.value;
-  });
+  const inputEl = ref(null);
+  const focusInput = () => {
+    inputEl.value.focus();
+  };
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
-
   <div class="m-3 w-25">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
     <input
-      v-model="name"
+      ref="inputEl"
       type="text"
       class="form-control"
       id="exampleInputEmail1"
-      aria-describedby="emailHelp"
     />
-    <div id="emailHelp" class="form-text">
-      We'll never share your email with anyone else.
-    </div>
+    <button @click="focusInput" type="button" class="btn btn-success">
+      focus
+    </button>
   </div>
-
-  <br />
-
-  <p class="m-3">{{ name }}</p>
-
-  <div
-    class="btn-group m-3"
-    role="group"
-    aria-label="Basic mixed styles example"
-  >
-    <button type="button" class="btn btn-danger" @click="num--">-</button>
-    <button type="button" class="btn btn-success" @click="num++">+</button>
-  </div>
-  <p class="m-3">{{ num }}</p>
-
-  <p>{{ getFullName }}</p>
-  <input class="form-control" type="text" v-model="firstName" />
 </template>
 
 <style>
