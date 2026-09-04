a736030428974bbaf4782abce92735f4a841ac1b
Amir-FF <amir.computer.04.ir@gmail.com>
2026-09-04 15:49:32 +0330

    add event

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,51 @@
 <script setup>
   import { ref } from "vue";
-  import "./css/main.css";
-  import "./sass/main.scss";
 
-  const age = 34;
-  const isAge = 40 < age;
+  const sayHello = (name, family, event) => {
+    console.log("say hello", name, family, event);
+  };
 
-  const style = ref({
-    color: isAge ? "red" : "blue",
-  });
-
-  const cla = ref({
-    yellow: isAge,
-    gray: true,
-  });
+  const keyHello = () => {
+    console.log("say hello");
+  };
 </script>
 
 <template>
-  <h1 :style="style">hello would</h1>
-
-  <h1 class="pink">hello would</h1>
-
-  <h1 class="bg-black" :class="[isAge ? cla : 'green', 'font-size-20px']">
-    hello would
-  </h1>
-
-  <h1 class="border">hello would</h1>
-
-  <h1 class="border-sass">hello would</h1>
-
-  <h1 class="border-scoped-sass">hello would</h1>
-
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+  <button
+    v-on:click.once="sayHello('Amir', 'Shamiri', $event)"
+    type="button"
+    class="btn btn-success m-2"
+  >
+    Success
+  </button>
+
+  <button
+    @click.stop="sayHello('Amir', 'Shamiri', $event)"
+    type="button"
+    class="btn btn-outline-warning"
+  >
+    Warning
+  </button>
+
+  <a @click.prevent href="#">
+    <button type="button" class="btn btn-danger ms-2">Danger</button>
+  </a>
+
+  <form class="w-50 m-2">
+    <div>
+      <label for="exampleInputEmail1" class="form-label">Email address</label>
+      <input
+        @keyup.alt.enter="keyHello"
+        type="email"
+        class="form-control"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+      />
+      <div id="emailHelp" class="form-text">
+        We'll never share your email with anyone else.
+      </div>
+    </div>
+  </form>
 </template>
-
-<style lang="scss" scoped>
-  $border-scoped: 2px #f00 solid;
-
-  .border-scoped-sass {
-    border: $border-scoped;
-  }
-
-  .pink {
-    color: pink;
-  }
-
-  .yellow {
-    color: yellow;
-  }
-
-  .gray {
-    color: gray;
-  }
-
-  .green {
-    color: green;
-  }
-
-  .bg-black {
-    background-color: black;
-  }
-
-  .font-size-20px {
-    font-size: 20px;
-  }
-</style>
